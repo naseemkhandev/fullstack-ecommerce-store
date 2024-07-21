8050058a7fb2e7d40149dc116e6ee9329c7b22a4
Naseem Khan <naseemsaffy@gmail.com>
2024-07-22 00:57:21 +0500

    feat: Add user registration with password hashing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,15 +1,27 @@
-export const register = (req, res) => {
+import bcrypt from "bcrypt";
+
+import createError from "../helpers/createError.js";
+import User from "../models/userModel.js";
+
+export const register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    const user = {
+    const userExists = await User.findOne({ email });
+    if (userExists) return next(createError(400, "User already registered!"));
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
       name,
       email,
-      password,
-    };
+      password: hashedPassword,
+    });
+
+    const { password: userPassword, ...user } = newUser._doc;
 
     res.status(201).json({ message: "User registered successfully!", user });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
