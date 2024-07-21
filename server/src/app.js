77a8145e5d951f2a647686c84d1a4f904c84ae6a
Naseem Khan <naseemsaffy@gmail.com>
2024-07-22 00:20:28 +0500

    chore: Update server configuration and middleware in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { config } from "./config/config.js";
 
 const app = express();
 
+// Middleware
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: config.clientUrl,
+    credentials: true,
+  })
+);
 
+// Routes
 app.get("/", (req, res) => {
   res.send("<h1>Server is running...</h1>");
 });
