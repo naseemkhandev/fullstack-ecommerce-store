f70879365d649a46dabc1282173a566c43a58f94
Naseem Khan <naseemsaffy@gmail.com>
2024-07-25 04:46:15 +0500

    feat: Add validation middleware for product creation

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 
 import { verifyAdmin } from "../middlewares/verifyToken.js";
 import { addNewProduct } from "../controllers/productController.js";
+import validateSchema from "../middlewares/validateSchema.js";
 
 const router = express.Router();
 
-router.post("/", verifyAdmin, addNewProduct);
+router.post("/", verifyAdmin, validateSchema(productSchemas), addNewProduct);
 
 export default router;
---
+++ server/src/validations/productValidation.js
@@ -0,0 +1,50 @@
+import { z } from "zod";
+
+const productSchema = z.object({
+  name: z
+    .string({ required_error: "Product Name is required" })
+    .nonempty({ message: "Product Name is required" })
+    .min(3, { message: "Product Name must be at least 3 characters long" })
+    .max(20, { message: "Product Name must be at most 20 characters long" })
+    .trim(),
+  description: z
+    .string({ required_error: "Product Description is required" })
+    .nonempty({ message: "Product Description is required" })
+    .min(10, {
+      message: "Product Description must be at least 10 characters long",
+    })
+    .max(200, {
+      message: "Product Description must be at most 200 characters long",
+    })
+    .trim(),
+  price: z
+    .number({ required_error: "Product Price is required" })
+    .nonempty({ message: "Product Price is required" })
+    .min(1, { message: "Product Price must be at least 1" })
+    .max(100000, { message: "Product Price must be at most 100000" })
+    .positive({ message: "Product Price must be greater than 0" }),
+  category: z
+    .string({ required_error: "Product Category is required" })
+    .nonempty({ message: "Product Category is required" })
+    .min(3, { message: "Product Category must be at least 3 characters long" })
+    .max(20, { message: "Product Category must be at most 20 characters long" })
+    .trim(),
+  stock: z
+    .number({ required_error: "Product Stock is required" })
+    .nonempty({ message: "Product Stock is required" })
+    .min(1, { message: "Product Stock must be at least 1" })
+    .max(1000, { message: "Product Stock must be at most 1000" })
+    .positive({ message: "Product Stock must be greater than 0" }),
+  sold: z
+    .number({ required_error: "Product Sold is required" })
+    .nonempty({ message: "Product Sold is required" })
+    .min(0, { message: "Product Sold must be at least 0" })
+    .max(1000, { message: "Product Sold must be at most 1000" })
+    .positive({ message: "Product Sold must be greater than 0" }),
+  images: z
+    .array(z.string({ required_error: "Product Images are required" }))
+    .nonempty({ message: "Product Images are required" })
+    .min(1, { message: "Product Images must be at least 1" }),
+});
+
+export default productSchema;
