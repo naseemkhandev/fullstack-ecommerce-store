ad7589f5503f9f6f4088e0a52d3222db7ab97ada
Naseem Khan <naseemsaffy@gmail.com>
2024-07-24 14:29:10 +0500

    feat: Add Navbar component

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,5 +1,109 @@
+import { CircleUser, Heart, Menu, Package2, ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { navLinks } from "../../constants/navLinks";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link to="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">ShopWave</span>
+            </Link>
+          </div>
+
+          {navLinks?.map((link) => (
+            <Link
+              key={link.id}
+              to={link.path}
+              className="text-muted-foreground transition-colors hover:text-foreground"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                <Link to="/" className="flex items-center gap-2 font-semibold">
+                  <Package2 className="h-6 w-6" />
+                  <span className="">ShopWave</span>
+                </Link>
+              </div>
+
+              {navLinks?.map((link) => (
+                <Link
+                  key={link.id}
+                  to={link.path}
+                  className="text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </nav>
+          </SheetContent>
+        </Sheet>
+
+        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          <div className="flex-center gap-1 ml-auto">
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Heart className="h-5 w-5" />
+            </Button>
+
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <ShoppingCart className="h-5 w-5" />
+            </Button>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full"
+                >
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ client/src/constants/navLinks.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: "Home",
+    path: "",
+  },
+  {
+    id: 2,
+    title: "Shop",
+    path: "/shop",
+  },
+  {
+    id: 5,
+    title: "Orders",
+    path: "/orders",
+  },
+  {
+    id: 3,
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: 4,
+    title: "Contact",
+    path: "/contact",
+  },
+];
