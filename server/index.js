dd5da9989fc5794c82030364470b1d21c7bde083
Naseem Khan <naseemsaffy@gmail.com>
2024-07-27 01:49:25 +0500

    refactor: Update CORS configuration in server/index.js

--- server/index.js
+++ server/index.js
@@ -19,12 +19,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: config.clientUrl,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
