85841f4054b17dcde4c4a7aa5f2f15cd9474f1e7
Naseem Khan <naseemsaffy@gmail.com>
2024-07-27 02:46:05 +0500

    refactor: Update CORS configuration in server/index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: config.clientUrl,
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
