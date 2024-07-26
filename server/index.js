31e06ff5ac09f93d375d64aaa7d2256ff193e5c0
Naseem Khan <naseemsaffy@gmail.com>
2024-07-27 01:53:19 +0500

    refactor: Add CORS middleware to server/index.js for enhanced security

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
