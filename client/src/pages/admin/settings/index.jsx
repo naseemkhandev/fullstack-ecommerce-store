6ddce7d241b96693628cdbd1e1813529482525bb
Naseem Khan <naseemsaffy@gmail.com>
2024-07-27 14:53:39 +0500

    chore: Update admin settings page with password update functionality

--- client/src/pages/admin/settings/index.jsx
+++ client/src/pages/admin/settings/index.jsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  const [settingsData, setSettingsData] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setSettingsData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="flex flex-col gap-5 max-w-lg">
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="oldPassword">
+          Old Password <span className="text-red-500">*</span>
+        </Label>
+
+        <Input
+          id="oldPassword"
+          name="oldPassword"
+          placeholder="Enter your old password"
+          value={settingsData?.oldPassword}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="newPassword">
+          New Password <span className="text-red-500">*</span>
+        </Label>
+
+        <Input
+          id="newPassword"
+          name="newPassword"
+          placeholder="Enter your new password"
+          value={settingsData?.oldPassword}
+          onChange={handleChange}
+        />
+      </div>
+
+      <Button type="submit">
+        Update Password
+      </Button>
+    </div>
+  );
 };
 
 export default SettingsPage;
