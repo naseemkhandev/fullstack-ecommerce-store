82283a67c3036b60274b9a2e97410410917291ac
Naseem Khan <naseemsaffy@gmail.com>
2024-07-30 13:53:54 +0500

    feat: Update productCard component with improved layout and functionality

--- client/src/components/products/productCard.jsx
+++ client/src/components/products/productCard.jsx
@@ -1,35 +1,47 @@
-import { Button } from "@/components/ui/button";
+import { Eye, Heart, ShoppingBasket, Star } from "lucide-react";
 
 const ProductCard = () => {
   return (
-    <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-      <div className="p-2">
+    <div className="relative flex w-full flex-col gap-5 rounded-sm bg-white bg-clip-border border text-gray-600 border-gray-100 group cursor-pointer">
+      <div className="bg-gray-50 border-b h-60 border-gray-100 relative overflow-hidden">
         <img
-          src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-          className="h-80 w-full rounded-xl object-cover"
+          src="https://img.freepik.com/free-psd/beautiful-gaming-headphone-isolated-transparent-background_191095-12528.jpg"
+          className="w-full rounded-t-sm object-contain max-h-60 group-hover:opacity-0 group-hover:scale-90 transition-all duration-500 delay-150"
         />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP6Ie5INr95uItxg0YVEe_eP0Bn6GOo63C_1VNfPP_0M7CuKDbeFw0Ddz62X5CcJK84tg&usqp=CAU"
+          className="w-full rounded-t-sm object-contain max-h-60 group-hover:opacity-100 opacity-0 group-hover:scale-110 transition-all duration-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-60"
+        />
+
+        <div className="absolute transition-all -bottom-6 group-hover:bottom-2 flex-center gap-1 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 duration-300 text-gray-500">
+          <Heart className="size-7 border p-1 hover:border-primary hover:bg-primary hover:text-white rounded bg-white cursor-pointer" />
+          <Eye className="size-7 border p-1 hover:border-primary hover:bg-primary hover:text-white rounded bg-white cursor-pointer" />
+          <ShoppingBasket className="size-7 border p-1 hover:border-primary hover:bg-primary hover:text-white rounded bg-white cursor-pointer" />
+        </div>
       </div>
 
-      <div className="p-3 flex flex-col gap-3">
-        <div className="mb-2 flex items-center justify-between">
-          <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
-            Apple AirPods
-          </p>
-          <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
-            $95.00
-          </p>
-        </div>
-        <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
-          With plenty of talk and listen time, voice-activated Siri access, and
-          an available wireless charging case.
+      <div className="p-3 flex flex-col gap-1">
+        <p className="block font-sans text-sm leading-normal">Electronics</p>
+
+        <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+          Apple Headphones Pro Max 2021 Edition - Black
         </p>
 
-        <Button
-          variant="outline"
-          className="w-full py-4"
-        >
-          Add to Cart
-        </Button>
+        <div className="flex items-center gap-1 mt-5">
+          {[...Array(5)].map((_, i) => (
+            <Star key={i} className="size-4 text-yellow-400 fill-yellow-400" />
+          ))}
+        </div>
+
+        <div className="mb-2 flex items-center gap-3 mt-2">
+          <p className="block font-sans text-base font-bold leading-relaxed text-blue-gray-900 antialiased">
+            $95.00
+          </p>
+
+          <s className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+            $120.00
+          </s>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <div className="container px-3 md:px-5 mx-auto mt-5">
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 xl:gap-5">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 xl:gap-5">
           {[...Array(10)].map((_, i) => (
             <ProductCard key={i} />
           ))}
