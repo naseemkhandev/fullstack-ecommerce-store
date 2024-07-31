90915d43142991d98ee78f26720782175675409f
Naseem Khan <naseemsaffy@gmail.com>
2024-07-31 16:58:59 +0500

    refactor: Update home page banner and hero section layout and responsiveness

--- client/src/pages/home/deals.jsx
+++ client/src/pages/home/deals.jsx
@@ -5,10 +5,10 @@ const Deals = () => {
     <div className="flex flex-col gap-10">
       <div className="flex-between items-end">
         <div>
-          <h2 className="text-3xl font-bold text-gray-700">
+          <h2 className="text-2xl md:text-3xl font-bold text-gray-700">
             Day of the <span className="text-primary">Deal</span>
           </h2>
-          <p className="text-base text-gray-500 mt-2">
+          <p className="text-sm md:text-base text-gray-500 mt-2">
             Don&apos;t wait. The time will never be just right.
           </p>
         </div>
