37e58b3d4c0d97551f8c4f12dff8228bd2e2c702
Naseem Khan <devnaseemkhan@gmail.com>
2024-08-27 16:53:02 +0500

    refactor: Update verifyToken middleware to handle token verification and user retrieval

--- server/src/middlewares/verifyToken.js
+++ server/src/middlewares/verifyToken.js
@@ -1,18 +1,20 @@
 import jwt from "jsonwebtoken";
-
 import { config } from "../config/config.js";
 import createError from "../helpers/createError.js";
 import User from "../models/userModel.js";
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    if (!token) next(createError(401, "You are not authenticated"));
+    if (!token) return next(createError(401, "You are not authenticated"));
 
-    jwt.verify(token, config.jwtSecret, async (err, user) => {
-      if (err) next(createError(403, "Token is not valid"));
+    jwt.verify(token, config.jwtSecret, async (err, decoded) => {
+      if (err) return next(createError(403, "Token is not valid"));
 
-      req.user = await User.findById(user.userId);
+      const user = await User.findById(decoded.userId);
+      if (!user) return next(createError(404, "User not found"));
+
+      req.user = user;
       next();
     });
   } catch (error) {
@@ -21,18 +23,22 @@ const verifyToken = (req, res, next) => {
 };
 
 const verifyUser = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.params.id === req.user.userId || req.user.isAdmin) {
+  verifyToken(req, res, async () => {
+    if (req.user && (req.params.id === req.user._id.toString() || req.user.isAdmin)) {
       next();
-    } else next(createError(403, "You are not authorized"));
+    } else {
+      next(createError(403, "You are not authorized"));
+    }
   });
 };
 
 const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
+  verifyToken(req, res, async () => {
+    if (req.user && req.user.isAdmin) {
       next();
-    } else next(createError(403, "You are not authorized"));
+    } else {
+      next(createError(403, "You are not authorized"));
+    }
   });
 };
 
